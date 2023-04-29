8fadab7517bbcee745bfed674172b962bc417114
TibowB <bruggeman.dev@gmail.com>
2023-04-29 17:58:19 +0200

    Add service worker

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './index.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './index.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/src/service-worker.js');
+  console.log('Service worker registered !');
+}
+
+createApp(App).mount('#app');
